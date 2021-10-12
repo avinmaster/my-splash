071b83b75f7a9327fe52f7ff701d6370b6355303
avinmaster <oybek.odilov.dev@gmail.com>
2021-10-12 14:51:16 +0500

    Setting up API for images index

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -14,17 +14,9 @@ class ImageController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $images = Image::simplePaginate(15);
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return $images;
     }
 
     /**
@@ -41,21 +33,10 @@ class ImageController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Image  $image
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Image $image)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Image  $image
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Image $image)
+    public function show($id)
     {
         //
     }
@@ -64,10 +45,10 @@ class ImageController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Image  $image
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Image $image)
+    public function update(Request $request, $id)
     {
         //
     }
@@ -75,10 +56,10 @@ class ImageController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Image  $image
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Image $image)
+    public function destroy($id)
     {
         //
     }
